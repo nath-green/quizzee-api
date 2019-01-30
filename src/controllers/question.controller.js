084e40cda112f52b17e4e494d783fb49098da344
Nathan Green <hello@nathgreen.co.uk>
2019-01-30 09:05:43 +0000

    removed unnecessary returned keys from controller

--- src/controllers/question.controller.js
+++ src/controllers/question.controller.js
@@ -4,9 +4,8 @@ exports.random = (req, res, next) => {
   const count = req.query.number ? parseInt(req.query.number) : 1;
 
   Question.aggregate([{ $sample: { size: count } }]).exec((err, data) => {
-    if (err)
-      return res.status(400).send({ data: [], success: false, message: 'Cannot return question' });
-    return res.status(200).send({ data, success: true, message: 'Question returned' });
+    if (err) return res.status(400).send({ message: 'Cannot return question' });
+    return res.status(200).send({ data });
   });
 };
 
@@ -15,18 +14,14 @@ exports.randomByCategory = (req, res, next) => {
   const { category } = req.params;
 
   Question.aggregate([{ $match: { category } }, { $sample: { size: count } }]).exec((err, data) => {
-    if (err)
-      return res.status(400).send({ data: [], success: false, message: 'Cannot return question' });
-    return res.status(200).send({ data, success: true, message: 'Question returned' });
+    if (err) return res.status(400).send({ message: 'Cannot return question' });
+    return res.status(200).send({ data });
   });
 };
 
 exports.add = (req, res, next) => {
   Question.create(req.body, (err, player) => {
-    if (err)
-      return res
-        .status(400)
-        .send({ data: {}, success: false, message: 'Question cannot be added' });
-    return res.status(200).send({ data: player, success: true, message: 'Question added' });
+    if (err) return res.status(400).send({ message: 'Question cannot be added' });
+    return res.status(200).send({ data: player });
   });
 };
